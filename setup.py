89a4a91174f000cded048365d9e13c20742c52aa
Shaun Tan <35413456+EvitanRelta@users.noreply.github.com>
2019-05-16 18:56:08 +0800

    Minor edits

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='pascal-voc-writer',
+    name='polygon-pascalvoc-writer',
     version='1.0',
-
     description='Simple module to write Pascal VOC xml files. Currently supports boundingBoxes and polygons.',
-
     url='https://github.com/EvitanRelta/polygon-pascalvoc-writer',
-
     author='EvitanRelta',
-
     license='MIT',
-
     keywords='polygon pascal voc annotation writer xml',
-
     packages=['polygon_pascalvoc_writer'],
-
     include_package_data=True,
-
     install_requires=['jinja2'],
 )
